782d0870fe63f933f2907306a42ddd21898bcc16
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-24 13:40:15 +0100

    add function MenuItem

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+import chesse from './images/chicken.jpg';
+import food from './images/food.jpeg';
+import jollof from './images/jollof.jpeg';
+import veg from './images/veg.jpeg';
+
+function MenuItem(name, src) {
+  this.name = name;
+  this.src = src;
+}
