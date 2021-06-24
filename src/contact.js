4b6e80531ac89e0bbb271ddbf6ef63c6269ab96c
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-24 13:31:48 +0100

    add function createContactSection

--- src/contact.js
+++ src/contact.js
@@ -4,3 +4,19 @@ const createTitle = () => {
   title.textContent = 'Contact';
   return title;
 };
+
+const createContactSection = () => {
+  const container = document.createElement('div');
+  container.classList.add('contact-section', 'mx-auto');
+  const phone = document.createElement('p');
+  phone.textContent = 'Phone: ###-####-####';
+  const email = document.createElement('p');
+  email.textContent = 'Email: email@email.com';
+  const location = document.createElement('p');
+  location.textContent = 'Location: 1337 Place, Location, LC';
+  container.append(phone);
+  container.append(email);
+  container.append(location);
+
+  return container;
+};
