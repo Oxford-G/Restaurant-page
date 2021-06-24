4554f7c9ba9d6efee84751f9be396812a8476285
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-24 13:37:20 +0100

    add modified index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("MMD")
+import './css/style.scss';
+import navbar from './nav';
+import homeFunc from './home';
+
+const content = document.querySelector('#content');
+const container = document.createElement('div');
+container.classList.add('main-container');
+navbar(content, container);
+content.appendChild(container);
+homeFunc(container);
